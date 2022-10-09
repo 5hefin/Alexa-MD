887b9afc4ec42a88b0f2ca869791c574b159825f
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-10-09 15:46:41 +0530

    fix

--- plugins/bgm.js
+++ plugins/bgm.js
@@ -13,7 +13,7 @@ bot(
     const array = ['Ayin','Ayinu','Bgm','Bot','Bye','Da','Good night','Hello','Hi','Neymar','Pm','Sed','alive','assist','ban','bgm','bot','converting','fake','fork','fuck','music','myre','njan','number','oombi','para','poda','raganork','remove','reply','sed','subscribe','umma','xxxtentation']
     array.map( async (a) => {
     let pattern = new RegExp(`\\b${a}\\b`, 'g');
-    if(pattern.test(message.text)){
+    if(pattern.test(message.message)){
     const Audio = await getBuffer('https://github.com/souravkl11/Raganork-legacy/blob/master/sourav/' + a + '.mp3?raw=true')
     try { var res = await toAudio(Audio, 'mp4') } catch(e) { return await message.sendMessage(`Error on parsing audio \n ${e}`) }
     return messsge.client.sendMessage(message.chat, { audio: Buffer.from(res.arrayBuffer), mimetype:'audio/mpeg', ptt: true }, { quoted: message.data })
