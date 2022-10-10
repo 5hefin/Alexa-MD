7ae0ea8cdd7129bbd5f9853b67ed956fade45909
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-10-10 22:17:33 +0530

    fix

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,77 +1,65 @@
-const { bot, getUrl, PluginDB, setPlugin, getPlugin } = require("../lib/");
+const {
+  bot,
+  PluginDB,
+  setPlugin,
+  getPlugin,
+} = require("../lib/");
+const fs = require("fs");
 const got = require("got");
-const axios = require('axios');
-const fs = require('fs');
-const { format } = require('util')
+const axios = require("axios");
 
 bot(
   {
-    pattern: "install",
+    pattern: "plugin ?(.*)",
     fromMe: true,
     desc: "Installs External plugins",
     type: "user"
   },
   async (message, match) => {
     match = match || message.reply_message.text
-    if (!match || !/\bhttps?:\/\/\S+/gi.test(match)) return await message.reply('_Need a URL!\n Example:_ .install https://gist.git....')
-    let links = match.match(/\bhttps?:\/\/\S+/gi);
-    for (let link of links) {
-      try {
-        var url = new URL(link);
-      } catch {
-        return await message.reply("_Invalid url!_");
-      }
-
-      if (url.host === "gist.github.com") {
-          url.host = "gist.githubusercontent.com";
-          url = url.toString() + "/raw"
-      } else {
-          url = url.toString()
-      }
-
-      try {
-        var response = await axios(url + "?timestamp=" + new Date());
-      } catch {
-        return await message.reply("_Invalid url!_")
-      }
-
-      let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data)
-      var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g) ? response.data.match(/pattern: ["'](.*)["'],/g).map(e => e.replace("pattern", "").replace(/[^a-zA-Z]/g, "")) : "temp"
-
-      try {
-        plugin_name = plugin_name[1].split(" ")[0]
-      } catch {
-        return await message.reply("_Invalid plugin. No plugin name found!_")
-      }
-
-      fs.writeFileSync("./plugins/" + plugin_name + ".js", response.data);
-
-      try {
-        require("./" + plugin_name);
-      } catch (e) {
-        fs.unlinkSync("/root/Alexa/plugins/" + plugin_name + ".js")
-        return await message.reply("_Error in plugin!_\n" + format(e));
-      }
-
-      await setPlugin(url, plugin_name);
-      await message.reply("_Newly installed plugin: " + plugin_name_temp.join(", ") + "_");
-    }
-  }
-);
-
-bot(
-  {
-    pattern: "plugin",
-    fromMe: true,
-    desc: "Plugin list",
-    type: "user",
-  },
-  async (message, match) => {
+    if (!match && match !== "list") return await message.send("_Example :*\nplugin url\nplugin list_")
+    if (match == 'list') {
     const plugins = await getPlugin();
     if (!plugins) return await message.reply("_Plugins not installed_");
     let msg = '';
     plugins.map((plugin) => { msg += '*' + plugin.dataValues.name + '* : ' + plugin.dataValues.url + '\n\n' });
     return await message.reply(msg);
+    }
+    let links = match.match(/\bhttps?:\/\/\S+/gi);
+    for (let link of links) {
+    try {
+      var url = new URL(link);
+    } catch {
+      return await message.reply("_Invalid url!_");
+    }
+    if (url.host === "gist.github.com") {
+        url.host = "gist.githubusercontent.com";
+        url = url.toString() + "/raw"
+    } else {
+        url = url.toString()
+    }
+    try {
+      var response = await axios(url + "?timestamp=" + new Date());
+    } catch {
+      return await message.reply("_Invalid url!_")
+    }
+    let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data)
+    var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g) ? response.data.match(/pattern: ["'](.*)["'],/g).map(e => e.replace("pattern", "").replace(/[^a-zA-Z]/g, "")) : "temp"
+    try {
+      plugin_name = plugin_name[1].split(" ")[0]
+    } catch {
+      return await message.reply("_Invalid plugin. No plugin name found!_")
+    }
+    fs.writeFileSync("./plugins/" + plugin_name + ".js", response.data);
+    try {
+      require("./" + plugin_name);
+    } catch (e) {
+      fs.unlinkSync("/root/Alexa/plugins/" + plugin_name + ".js")
+      return await message.reply("_Error in plugin!_\n" + format(e));
+    }
+    await setPlugin(url, plugin_name);
+    await message.reply("_Newly installed plugin: " + plugin_name_temp.join(", ") + "_");
+    }
   }
 );
 
@@ -84,16 +72,12 @@ bot(
   },
   async (message, match) => {
     if (!match) return await message.sendMessage("_Need a plugin name_");
-
     var plugin = await PluginDB.findAll({ where: { name: match } });
-
-    if (plugin.length < 1) {
-      return await message.sendMessage("_Plugin not found_");
-    } else {
-      await plugin[0].destroy();
-      delete require.cache[require.resolve("./" + match + ".js")];
-      fs.unlinkSync("./plugins/" + match + ".js");
-      await message.sendMessage(`Plugin ${match} deleted`);
+    if (plugin.length < 1) { return await message.sendMessage("_Plugin not found_"); } else {
+    await plugin[0].destroy();
+    delete require.cache[require.resolve("./" + match + ".js")];
+    fs.unlinkSync("./plugins/" + match + ".js");
+    await message.sendMessage(`Plugin ${match} deleted`);
     }
   }
 );
