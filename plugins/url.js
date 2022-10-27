92f0bab54357bac268f24645b8fcb16b20c66851
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-10-27 18:55:49 +0530

    added url cmd

---
+++ plugins/url.js
@@ -0,0 +1,16 @@
+const { bot, upload, isPublic } = require("../lib/");
+
+bot(
+  {
+    pattern: "url ?(.*)",
+    fromMe: isPublic,
+    desc: "upload files to imgur.com",
+    type: "media",
+  },
+  async (message, match) => {
+    if (message.reply_message.image || message.reply_message.video) return await m.sendReply("_Reply to image|video_");
+    var shefin = await message.reply_message.downloadMediaMessage();
+    var res = await upload(shefin);
+    await message.reply(res.url)
+  }
+);
