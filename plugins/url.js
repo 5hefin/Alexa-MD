14273ae13f07bb67d99edfb82d641d3959f59c17
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-10-29 12:17:35 +0530

    fix

--- plugins/url.js
+++ plugins/url.js
@@ -11,6 +11,6 @@ bot(
     if (!message.reply_message.image || message.reply_message.video) return await message.reply("_Reply to image|video_");
     var media = await message.reply_message.downloadMediaMessage();
     var res = await upload(media);
-    await message.reply(res.url)
+    await message.reply(res.link)
   }
 );
