dd16bf03f07efbfbdeae334152a4a57eea47e0fe
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-10-29 13:27:30 +0530

    fix

--- plugins/url.js
+++ plugins/url.js
@@ -1,4 +1,5 @@
 const { bot, upload, isPublic } = require("../lib/");
+const ffmpeg = require('fluent-ffmpeg');
 
 bot(
   {
@@ -8,9 +9,28 @@ bot(
     type: "media",
   },
   async (message, match) => {
-    if (!message.reply_message.image || message.reply_message.video) return await message.reply("_Reply to image|video_");
-    var media = await message.reply_message.downloadMediaMessage();
-    var res = await upload(media);
-    await message.reply(res.link)
+    if (message.reply_message.audio) {
+      try {
+        var media = await message.reply_message.downloadMediaMessage();
+        ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
+        var res = await upload('output.mp4');
+        await message.reply(res.link);
+        });
+      } catch (e) {
+	await message.reply(e.message)
+      }
+    }
+
+    else if (message.reply_message.image || message.reply_message.video) {
+      try {
+        var media = await message.reply_message.downloadMediaMessage();
+        var res = await upload(media);
+        await message.reply(res.link)
+      } catch (e) {
+	await message.reply(e.message)
+      }
+    }
+
+    else return await message.reply("_Reply to image/video/audio_");
   }
 );
