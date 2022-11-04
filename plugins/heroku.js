7ca679c53b16b241c1fa6347da4985cde308d488
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-04 18:12:21 +0530

    Update heroku.js

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -130,10 +130,13 @@ bot(
   },
   async (message) => {
     const { HANDLERS } = require("../config");
+    var sn;
+    if (HANDLERS == "null") sn = "";
+    else sn = HANDLERS.split("")[0] 
     const prefix = HANDLERS !== "null" ? HANDLERS.split("")[0] : "";
     const buttons = [
-      {buttonId: prefix + 'setvar MODE:public', buttonText: {displayText: 'Public'}, type: 1},
-      {buttonId: prefix + 'setvar MODE:private', buttonText: {displayText: 'Private'}, type: 1}
+      {buttonId: sn + 'setvar MODE:public', buttonText: {displayText: 'Public'}, type: 1},
+      {buttonId: sn + 'setvar MODE:private', buttonText: {displayText: 'Private'}, type: 1}
     ]
     await sendButton(buttons, "*Working mode control panel*", "Bot is currently running on "+config.MODE+" mode now", message)
   }
