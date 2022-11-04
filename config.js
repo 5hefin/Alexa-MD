76b7c59ea5ba44cd7933f515d54f82fe8bdfd3c0
5hefin <shefinfranklin88@gmail.com>
2022-11-04 11:06:10 +0530

    v2.0.0

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
 module.exports = {
-    VERSION: 'v1.0.0',
+    VERSION: 'v2.0.0',
     SESSION_ID: (process.env.SESSION_ID || '').trim(),
     MODE: process.env.MODE || 'private',
     DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
