626aff64a68f79e90777b4ae1d0d907bad7dc111
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-04 14:08:50 +0530

    Update cmd.js

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -1,9 +1,11 @@
 const {
    bot,
-   setCmd,
-   prepareCmd,
    isPublic,
 } = require("../lib/");
+const {
+   setCmd,
+   prepareCmd,
+} = require("../lib/cmd");
 
 bot(
   {
@@ -15,7 +17,6 @@ bot(
   async (message, match) => {
     var text = match
     var m = message
-    if (!message.reply_messge) return await m.reply('_Reply to a image/video/audio/sticker_')
     if (!text) return await m.reply('_Example : setcmd ping_')
     const setcmd = await setCmd(m, text);
     if (!setcmd) return await m.reply('_Failed_')
