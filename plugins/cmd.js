68be31619dd7a1d77f588ffb920edf4cac226eac
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-04 15:03:22 +0530

    Update cmd.js

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -14,9 +14,9 @@ bot(
     desc: 'to set audio/image/video as a cmd',
     type: 'misc'
   },
-  async (message, match) => {
+  async (message, match, m) => {
     var text = match
-    var m = message
+    var m = m || message
     if (!text) return await m.reply('_Example : setcmd ping_')
     const setcmd = await setCmd(m, text);
     if (!setcmd) return await m.reply('_Failed_')
