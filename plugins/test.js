c5f7f9a38e0884ac5d06fac1c3b3e3ae3c557cf2
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-04 16:08:21 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,25 @@
+const {
+    isAdmin,
+    mentionjid
+} = require('../lib/admin');
+const {
+    bot
+} = require('../lib')
+
+bot({
+    pattern: 'promote ?(.*)',
+    fromMe: true,
+    type: 'group',
+    desc: "test"
+}, async (message, match) => {
+    const user = message.mention[0] || message.reply_message.jid
+    if (!user) return await message.reply("Test")
+    if (!message.isGroup) return await message.reply("group")
+    var admin = await isAdmin(message);
+    if (!admin) return await message.reply("not edmin")
+    await message.client.sendMessage(message.jid, {
+        text: mentionjid(user) + "Promote",
+        mentions: [user]
+    })
+    await message.client.groupParticipantsUpdate(message.jid, [user], "promote")
+})
