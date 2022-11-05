df67a3c5bd2f1c20743cc7838cd7143ec0fc5f27
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-05 07:15:29 +0530

    Create group.js

---
+++ plugins/group.js
@@ -0,0 +1,112 @@
+const {
+    bot,
+    isAdmin,
+    parsedJid
+} = require("../lib");
+const config = require("../config");
+
+bot(
+  {
+    pattern: "add",
+    fromMe: true,
+    desc: "Adds a person to group",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention user to add");
+    let isadmin = await isAdmin(message.jid, message.user, message.client);
+    if (!isadmin) return await message.reply("_I'm not admin_");
+    let jid = parsedJid(match);
+    await message.add(jid);
+    return await message.reply(`@${jid[0].split("@")[0]} added`, { mentions: jid, });
+  }
+);
+
+bot(
+  {
+    pattern: "kick",
+    fromMe: true,
+    desc: "kicks a person from group",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention user to kick");
+    let isadmin = await isAdmin(message.jid, message.user, message.client);
+    if (!isadmin) return await message.reply("_I'm not admin_");
+    let jid = parsedJid(match);
+    await message.kick(jid);
+    return await message.reply(`@${jid[0].split("@")[0]} kicked`, { mentions: jid, });
+  }
+);
+
+bot(
+  {
+    pattern: "promote",
+    fromMe: true,
+    desc: "promote a member",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention user to promote_");
+    let isadmin = await isAdmin(message.jid, message.user, message.client);
+    if (!isadmin) return await message.reply("_I'm not admin_");
+    let jid = parsedJid(match);
+    await message.promote(jid);
+    return await message.reply(`@${jid[0].split("@")[0]} promoted as admin`, { mentions: jid, });
+  }
+);
+
+bot(
+  {
+    pattern: "demote",
+    fromMe: true,
+    desc: "demote a member",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention user to demote");
+    let isadmin = await isAdmin(message.jid, message.user, message.client);
+    if (!isadmin) return await message.reply("_I'm not admin_");
+    let jid = parsedJid(match);
+    await message.demote(jid);
+    return await message.reply(`@${jid[0].split("@")[0]} demoted from admin`, { mentions: jid, });
+  }
+);
+
+bot(
+  {
+    pattern: "mute",
+    fromMe: true,
+    desc: "mute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    if (!isAdmin(message.jid, message.user, message.client)) return await message.reply("_I'm not admin_");
+    await message.reply("_Muting_");
+    return await client.groupSettingUpdate(message.jid, "announcement");
+  }
+);
+
+bot(
+  {
+    pattern: "unmute",
+    fromMe: true,
+    desc: "unmute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup) return await message.reply("_This bot is for groups_");
+    if (!isAdmin(message.jid, message.user, message.client)) return await message.reply("_I'm not admin_");
+    await message.reply("_Unmuting_");
+    return await client.groupSettingUpdate(message.jid, "not_announcement");
+  }
+);
