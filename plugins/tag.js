b810b75e937e7498a11584cac1ae4c7fc9c72e93
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-18 16:49:55 +0530

    fixed tag

--- plugins/tag.js
+++ plugins/tag.js
@@ -1,4 +1,7 @@
-const { bot, parseJid } = require("../lib/");
+const {
+  bot,
+  menJid,
+} = require("../lib/");
 
 bot(
   {
@@ -8,23 +11,40 @@ bot(
     type: "group",
   },
   async (message, match) => {
-    if (!message.isGroup) return;
     const { participants } = await message.client.groupMetadata(message.jid);
+    const mentionedJid = await menJid(participants)
     if (match == "all") {
       let msg = ""
       let count = 1
-      for (let participant of participants) {
-        msg += `${count++} @${participant.id.split('@')[0]}\n`
-      }
-      return await message.reply(msg, { mentions: participants.map((a) => a.id) });
+      mentionedJid.forEach((e) =>(msg += `${count++} @${e.split("@")[0]}\n`))
+      return await message.sendMessage(msg.trim(), { mentions: mentionedJid })
     } else if (match == "admin" || match == "admins") {
-      let admins = message.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : "";
       let msg = ""
-      let count = 1
-      for (let admin of admins) {
-        msg += `${count++} @${admin.split('@')[0]}\n`
-      }
-      return await message.reply(msg, { mentions: parseJid(msg) });
+      let mentionedJid = participants.filter((user) => !!user.admin == true).map(({ id }) => id)
+      mentionedJid.forEach((e) => (msg += `@${e.split("@")[0]}\n`))
+      return await message.sendMessage(msg.trim(), { mentions: mentionedJid })
+    } else if (match == "notadmin" || match == "not admins") {
+      let msg = ""
+      const mentionedJid = participants.filter((user) => !!user.admin != true).map(({ id }) => id)
+      mentionedJid.forEach((e) => (msg += `@${e.split("@")[0]}\n`))
+      return await message.sendMessage(msg.trim(), { mentions: mentionedJid })
+    }
+    if (match || message.reply_message.text) {
+      return await message.sendMessage(match || message.reply_message.text, { mentions: mentionedJid })
+    } else if (message.reply_message.image) {
+      const media = await message.reply_message.download()
+      return await message.sendMessage(media, { mentions: mentionedJid }, "image");
+    } else if (message.reply_message.video) {
+      const media = await message.reply_message.download()
+      return await message.sendMessage(media, { mentions: mentionedJid }, "video");
+    } else if (message.reply_message.audio) {
+      const media = await message.reply_message.download()
+      return await message.sendMessage(media, { mimetype: "audio/mpeg", mentions: mentionedJid }, "audio")
+    } else if (message.reply_message.sticker) {
+      const media = await message.reply_message.download()
+      return await message.sendMessage(media, { packname: "Alexa MD", mentions: mentionedJid }, "sticker");
+    } else if (!message.reply_message) {
+      return await message.sendMessage("*_Example :_*\n_tag all_\n_tag admin_\n_tag notadmin_\n_tag text_\n_Reply to a message_")
     }
   }
 );
