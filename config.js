565308540c38ce29bd2539e81bf1f67f80507191
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-20 14:25:50 +0530

    fix

--- config.js
+++ config.js
@@ -1,25 +1,24 @@
-const toBool = (x) => x == 'true'
-const { Sequelize } = require('sequelize')
-const { existsSync } = require('fs')
-if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
-const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
+const toBool = (x) => x == "true"
+const { Sequelize } = require("sequelize")
+const { existsSync } = require("fs")
+if (existsSync("config.env")) require("dotenv").config({ path: "./config.env" })
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
 module.exports = {
-    VERSION: 'v2.0.0',
-    SESSION_ID: (process.env.SESSION_ID || '').trim(),
-    MODE: process.env.MODE || 'private',
-    DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
-    HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-    SUDO: process.env.SUDO || '',
+    VERSION: require("./package.json").version,
+    SESSION_ID: (process.env.SESSION_ID || "").trim(),
+    MODE: process.env.MODE || "private",
+    DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
+    HANDLERS: (process.env.PREFIX || "^[.,!]").trim(),
+    SUDO: process.env.SUDO || "",
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
     HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    AUDIO_DATA: process.env.AUDIO_DATA || '𝛥𝐿𝛯𝛸𝛥,𝑆𝛨𝛯𝐹𝛪𝛮,https://i.imgur.com/daBdQPW.jpeg',
-    BOT_INFO: process.env.BOT_INFO || '𝛥𝐿𝛯𝛸𝛥,𝑆𝛨𝛯𝐹𝛪𝛮,919567489404,https://i.imgur.com/daBdQPW.jpeg',
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Alexa,Shefin',
-    READ_CMD: process.env.READ_COMMAND || 'true', 
+    AUDIO_DATA: process.env.AUDIO_DATA || "𝛥𝐿𝛯𝛸𝛥,𝑆𝛨𝛯𝐹𝛪𝛮,https://i.imgur.com/daBdQPW.jpeg",
+    BOT_INFO: process.env.BOT_INFO || "𝛥𝐿𝛯𝛸𝛥,𝑆𝛨𝛯𝐹𝛪𝛮,919567489404,https://i.imgur.com/daBdQPW.jpeg",
+    STICKER_DATA: process.env.STICKER_DATA || "Alexa,Shefin",
+    READ_CMD: process.env.READ_COMMAND || "true", 
     LOG_MSG: toBool(process.env.LOG_MSG),
     ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
-    LANG: (process.env.LANGUAGE || 'english').toLowerCase(),
-    WARN_LIMIT: process.env.WARN_LIMIT || '3',
-    DIS_BOT: process.env.DISABLE_BOT || 'null',
+    WARN_LIMIT: process.env.WARN_LIMIT || "3",
+    DIS_BOT: process.env.DISABLE_BOT || "null",
     ERROR_MESSAGE: toBool(process.env.ERROR_MESSAGE),
 };
