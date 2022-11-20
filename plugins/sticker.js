88aba62bd8e568fb2e5bd306f4735554f846a3eb
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-20 14:27:16 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -3,7 +3,7 @@ const {
   isPublic,
 } = require("../lib/");
 const {
-  STICKER_PACKNAME
+  STICKER_DATA
 } = require("../config");
 
 bot(
@@ -17,7 +17,7 @@ bot(
     if (!message.reply_message || (!message.reply_message.video && !message.reply_message.image)) return await message.reply("_Reply to image/video_")
     let media = await message.reply_message.download();
     let [p, a] = match.split(",");
-    let [s, n] = STICKER_PACKNAME.split(",");
+    let [s, n] = STICKER_DATA.split(",");
     await message.sendMessage(media, { packname: p || s, author: a || n }, "sticker");
   }
 );
@@ -33,7 +33,7 @@ bot(
     if (!message.reply_message || !message.reply_message.sticker) return await message.reply("_Reply to sticker_");
     let media = await message.reply_message.download();
     let [p, a] = match.split(",");
-    let [s, n] = STICKER_PACKNAME.split(",");
+    let [s, n] = STICKER_DATA.split(",");
     await message.sendMessage(media, { packname: p || s, author: a || n }, "sticker");
   }
 );
