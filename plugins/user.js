51d27c91773613cf960910bb1983fac7b2ab71f2
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-11-22 18:03:04 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -164,3 +164,21 @@ bot(
     await message.reply("_Unarchived_")
   }
 );
+
+bot(
+  {
+    pattern: "onwa ?(.*)",
+    fromMe: true,
+    desc: "To check if a given ID is on WhatsApp.",
+    type: "whatsapp",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text
+    if (!match) return await message.reply("_Need Number!_")
+    match = match.replace(/[^0-9]/g, "")
+    if (!match) return await message.reply("_Need Number!_")
+    const [result] = await message.client.onWhatsApp(match)
+    if (!result) await message.reply(match + " doest exists on WhatsApp")
+    if (result && result.exists) return await message.reply("*" + match + " exists on WhatsApp*,\njid: " + result.jid)
+  }
+)
